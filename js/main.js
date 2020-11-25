5531e792cbeb7986fb2628d64d56d2a911570918
Ivan <delfatti.ivan@gmail.com>
2020-11-25 12:25:33 +0100

    Milestone 1

--- js/main.js
+++ js/main.js
@@ -10,6 +10,42 @@ const boolFlix = new Vue ({
 
         search:'',
 
+        movieList: [],
+
+    },
+
+    methods: {
+
+        filterSearch(){
+
+            if ( this.search.length >= 3 ) {
+                this.movies()
+            }
+
+
+
+        },
+
+        movies() {
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+                params: {
+                    api_key: '60978fe417c91cef357193610fbcdac9',
+                    language: 'it-IT',
+                    query: this.search
+
+                }
+            })
+            .then( valid => {
+                // handle success
+                this.movieList = valid.data.results
+                console.log(valid.data.results);
+              })
+              .catch( invalid => {
+                // handle error
+                console.log(invalid);
+              })
+        }
+
     },
 
 });
